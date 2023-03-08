ce8bd512721a8ca8a1f37f6ae4180968699aa86b
ycheng289 <ycheng289@GApps.cityu.edu.hk>
2023-03-08 15:01:42 +0800

    I Can WIN AI DAMN

--- python/AIPlayer.py
+++ python/AIPlayer.py
@@ -9,13 +9,6 @@ import copy
 from math import inf as infinity
 import random
 
-'''
-# Version 3
-# 1. Valid Move
-# 2. Adjacent Move + Length
-# 3. Random
-'''
-
 
 class AIPlayer(object):
     def __init__(self, name, symbole, isAI=False):
@@ -52,13 +45,21 @@ class AIPlayer(object):
         return cells
 
     def get_move(self, state, player):
+        '''
+        Description:
+        1. Find available cells from the state using the given function available_cells()
+        2. Obtain the Valid Moves for AI that it will only select its valid cells (black or white depends on player)
+        3. For each cell in Valid move, count its neigbors
+            3.1 if number of neigbor = 2 or 5, -> select the cell
+            3.2 if number of neigbor != 2 and != 5 -> find valid move from its neigbors using the function get_neigbor()
+                then select random cells in the obtained neigbor array
+        '''
 
         ##########################################################
         print("\nBoard: \n", state)
         ##########################################################
 
         # Array: Get Valid Move
-
         def valid_move(games):
             correct_cells = []
             for i in games:
@@ -72,97 +73,112 @@ class AIPlayer(object):
             ##########################################################
             print("\nCorrect Moves: \n", correct_cells)
             ##########################################################
+
             return correct_cells
 
         def get_neigbor(state, valid_move):
-            good_move = []
+
+            hasNeigbor = []
+
             for i in valid_move:
                 if (i[0] != 0 and i[1] != 0 and i[0] != 5 and i[1] != 5):
                     if (state[i[0]-1][i[1]] != None) or (state[i[0]][i[1]-1] != None) or (state[i[0]+1][i[1]] != None) or (state[i[0]][i[1]+1] != None):
-                        good_move.append(i)
+                        hasNeigbor.append(i)
                 elif i[0] == 0 and i[1] == 0:
                     if (state[i[0]+1][i[1]] != None) or (state[i[0]][i[1]+1] != None):
-                        good_move.append(i)
+                        hasNeigbor.append(i)
                 elif i[0] == 0 and i[1] != 0:
                     if (i[1] < 5):
                         if (state[i[0]][i[1]-1] != None) or (state[i[0]+1][i[1]] != None) or (state[i[0]][i[1]+1] != None):
-                            good_move.append(i)
+                            hasNeigbor.append(i)
                     else:
                         if (state[i[0]][i[1]-1] != None) or (state[i[0]+1][i[1]] != None):
-                            good_move.append(i)
+                            hasNeigbor.append(i)
                 elif i[0] != 0 and i[1] == 0:
                     if (i[0] < 5):
                         if (state[i[0]-1][i[1]] != None) or (state[i[0]+1][i[1]] != None) or (state[i[0]][i[1]+1] != None):
-                            good_move.append(i)
+                            hasNeigbor.append(i)
                     else:
                         if (state[i[0]-1][i[1]] != None) or (state[i[0]][i[1]+1] != None):
-                            good_move.append(i)
+                            hasNeigbor.append(i)
 
-            return good_move
+            return hasNeigbor
+
+        def countNeigbor(state, validMoves):
+
+            pos_h_v = []
+
+            for i in validMoves:
+
+                vertical_cnt = 0
+                horizontal_cnt = 0
+
+                x = i[0]
+                y = i[1]
+
+                for k in range(0, x):
+                    if (state[k, y] != None):
+                        horizontal_cnt += 1
+                    else:
+                        horizontal_cnt = 0
+
+                for k in range(x + 1, 6):
+                    if (state[k, y] != None):
+                        horizontal_cnt += 1
+                    else:
+                        horizontal_cnt = 0
+
+                for k in range(0, y):
+                    if (state[x, k] != None):
+                        vertical_cnt += 1
+                    else:
+                        vertical_cnt = 0
+
+                for k in range(6, y + 1):
+                    if (state[x, k] != None):
+                        vertical_cnt += 1
+                    else:
+                        vertical_cnt = 0
+
+                pos_h_v.append((i, horizontal_cnt, vertical_cnt))
+
+            return pos_h_v
+
+        def chooseBestMove(neigborCount):
+            bestMove = None
+            for i in neigborCount:
+                if i[1] == 5 or i[2] == 5:
+                    bestMove = i[0]
+                    ##########################################################
+                    print("Best Move with 5: ", bestMove)
+                    ##########################################################
+                    break
+                elif i[1] == 2 or i[2] == 2:
+                    bestMove = i[0]
+                    ##########################################################
+                    print("Best Move with 2: ", bestMove)
+                    ##########################################################
+                    break
+            return bestMove
 
         games = self.available_cells(state, player)
         validMoves = valid_move(games)
-        pos_h_v = []
-
-        for i in validMoves:
-
-            vertical_cnt = 0
-            horizontal_cnt = 0
-
-            x = i[0]
-            y = i[1]
-
-            for k in range(0, x):
-                if (state[k, y] != None):
-                    horizontal_cnt += 1
-                else:
-                    horizontal_cnt = 0
-
-            for k in range(x + 1, 6):
-                if (state[k, y] != None):
-                    horizontal_cnt += 1
-                else:
-                    horizontal_cnt = 0
-
-            for k in range(0, y):
-                if (state[x, k] != None):
-                    vertical_cnt += 1
-                else:
-                    vertical_cnt = 0
-
-            for k in range(6, y + 1):
-                if (state[x, k] != None):
-                    vertical_cnt += 1
-                else:
-                    vertical_cnt = 0
-
-            pos_h_v.append((i, horizontal_cnt, vertical_cnt))
+        neigborCount = countNeigbor(state, validMoves)
 
         #####################################
         print("\nPOS HORIZONTAL VERTICAL: \n")
-        for i in pos_h_v:
+        for i in neigborCount:
             print(i)
         #####################################
 
-        bestMove = None
-
-        for i in pos_h_v:
-            if i[1] == 5 or i[2] == 5:
-                bestMove = i[0]
-                ##########################################################
-                print("Best Move with 5: ", bestMove)
-                ##########################################################
-                break
-            elif i[1] == 2 or i[2] == 2:
-                bestMove = i[0]
-                ##########################################################
-                print("Best Move with 2: ", bestMove)
-                ##########################################################
-                break
+        bestMove = chooseBestMove(neigborCount)
 
         if bestMove == None:
             goodMove = get_neigbor(state, validMoves)
-            bestMove = random.choice(goodMove)
+            if (len(goodMove) == 0):
+                bestMove = random.choice(validMoves)
+            else:
+                bestMove = random.choice(goodMove)
             ##########################################################
             print("Best Move with random: ", bestMove)
             ##########################################################
